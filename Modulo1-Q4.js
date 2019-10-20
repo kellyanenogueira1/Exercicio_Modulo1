f11bb9b2e327521fe8010b22a900c8850778e9e8
Kellyane Nogueira <kellyanenogueira1@gmail.com>
2019-10-20 16:38:31 -0300

    Usando o destructuring para acessar um objeto

---
+++ Modulo1-Q4.js
@@ -0,0 +1,11 @@
+const fisica = {
+    nome: 'Física',
+    alunos: ['Iago', 'João'],
+    localizacao: {
+        bloco: 3,
+        sala: 4
+    }
+}
+
+const { localizacao: {sala}} = fisica
+console.log(sala)
