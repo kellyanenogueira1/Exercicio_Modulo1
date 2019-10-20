766003bb16d6e09101acc1b1b34a87f179fb2df2
Kellyane Nogueira <kellyanenogueira1@gmail.com>
2019-10-20 16:40:40 -0300

    Acesso ao atributo usando a forma declarativa

---
+++ Modulo1-Q5.js
@@ -0,0 +1,12 @@
+const aluno = {
+    nome: 'iago',
+    notas: [
+        { nome: 'fisica', valor: 5 },
+        { nome: 'estatistica', valor: 4 },
+        { nome: 'probabilidade', valor: 1 },
+    ]
+}
+
+console.log(aluno.notas[0].valor)
+console.log(aluno.notas[1].valor)
+console.log(aluno.notas[2].valor)
